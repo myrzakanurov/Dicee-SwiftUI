7d73b9d3a229273c0de7841969db589f509ef724
Myrzakanurov Arman <myrzakanurov.a@gmail.com>
2021-03-19 16:48:16 +0600

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Arman Myrzakanurov on 19.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
